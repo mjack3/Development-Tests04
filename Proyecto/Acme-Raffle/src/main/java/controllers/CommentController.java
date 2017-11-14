3ad57c4ea561df1eeb9a1154f0c57733061a83fb
migcamrom1 <migcamrom1@1fa25251-71d6-4f2b-80d7-3f32b33dfe7f>
2017-11-14 18:29:44 +0000

    haciendo comentar

--- Proyecto/Acme-Raffle/src/main/java/controllers/CommentController.java
+++ Proyecto/Acme-Raffle/src/main/java/controllers/CommentController.java
@@ -1,3 +1,4 @@
+
 package controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,12 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import domain.Administrator;
-import domain.Auditor;
-import domain.Manager;
-import domain.Prize;
-import domain.Raffle;
-import domain.User;
 import services.AdministratorService;
 import services.AuditorService;
 import services.CommentService;
@@ -20,58 +15,88 @@ import services.ManagerService;
 import services.PrizeService;
 import services.RaffleService;
 import services.UserService;
+import domain.Administrator;
+import domain.Auditor;
+import domain.Comment;
+import domain.Manager;
+import domain.Prize;
+import domain.Raffle;
+import domain.User;
 
 @RequestMapping("/comment")
 @Controller
 public class CommentController {
-	
-	@Autowired
-	private CommentService	commentService;
-	
-	@Autowired
-	private AuditorService	auditorService;
-	
-	@Autowired
-	private RaffleService	raffleService;
-	
-	@Autowired
-	private PrizeService	prizeService;
-	
-	@Autowired
-	private ManagerService	managerService;
-	
-	@Autowired
-	private UserService		userService;
-	
-	@Autowired
-	private AdministratorService		administratorService;
 
-	
+	@Autowired
+	private CommentService			commentService;
+
+	@Autowired
+	private AuditorService			auditorService;
+
+	@Autowired
+	private RaffleService			raffleService;
+
+	@Autowired
+	private PrizeService			prizeService;
+
+	@Autowired
+	private ManagerService			managerService;
+
+	@Autowired
+	private UserService				userService;
+
+	@Autowired
+	private AdministratorService	administratorService;
+
+
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public ModelAndView list(@RequestParam Integer q) {
+	public ModelAndView list(@RequestParam final Integer q) {
 		ModelAndView result;
 
-		Auditor auditor = auditorService.findOne(q);
-		Manager manager = managerService.findOne(q);
-		User user = userService.findOne(q);
-		Administrator admin = administratorService.findOne(q);
-		Raffle raffle = raffleService.findOne(q);
-		Prize prize= prizeService.findOne(q);
+		final Auditor auditor = this.auditorService.findOne(q);
+		final Manager manager = this.managerService.findOne(q);
+		final User user = this.userService.findOne(q);
+		final Administrator admin = this.administratorService.findOne(q);
+		final Raffle raffle = this.raffleService.findOne(q);
+		final Prize prize = this.prizeService.findOne(q);
 		result = new ModelAndView("comment/list");
-		
-		if(auditor!=null)
+
+		if (auditor != null)
 			result.addObject("comments", auditor.getComments());
-		if(manager!=null)
+		if (manager != null)
 			result.addObject("comments", manager.getComments());
-		if(user!=null)
+		if (user != null)
 			result.addObject("comments", user.getComments());
-		if(admin!=null)
+		if (admin != null)
 			result.addObject("comments", admin.getComments());
-		if(raffle!=null)
+		if (raffle != null)
 			result.addObject("comments", raffle.getComments());
-		if(prize!=null)
+		if (prize != null)
 			result.addObject("comments", prize.getComments());
 
 		return result;
 	}
+	@RequestMapping(value = "create", method = RequestMethod.GET)
+	public ModelAndView create(@RequestParam final int raffleId) {
+
+		ModelAndView resul;
+		final Raffle raffle = this.raffleService.findOne(raffleId);
+		final Comment comment = this.commentService.create();
+		comment.setRaffle(raffle);
+		resul = this.createEditModelAndView(comment, null);
+
+		return resul;
+	}
+
+	private ModelAndView createEditModelAndView(final Comment comment, final String message) {
+		// TODO Auto-generated method stub
+		final ModelAndView resul = new ModelAndView("comment/edit");
+		resul.addObject("comment", comment);
+		resul.addObject("message", message);
+
+		resul.addObject("actionParam", "comment/edit.do");
+
+		return resul;
+	}
+
 }
