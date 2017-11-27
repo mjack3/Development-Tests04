23ae4639ef7e6eb187e73fa8c9edde5029ad8039
migcamrom1 <migcamrom1@1fa25251-71d6-4f2b-80d7-3f32b33dfe7f>
2017-11-27 14:35:42 +0000

    junits arreglos

--- Acme-Raffle/src/test/java/services/AuditReportTest.java
+++ Acme-Raffle/src/test/java/services/AuditReportTest.java
@@ -24,6 +24,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import utilities.AbstractTest;
 import domain.AuditReport;
 import domain.Raffle;
+
 @Transactional
 @ContextConfiguration(locations = {
 	"classpath:spring/junit.xml"
@@ -34,107 +35,99 @@ public class AuditReportTest extends AbstractTest {
 	@Autowired
 	private AuditReportService	auditReportService;
 	@Autowired
-	private RaffleService	raffleService;
-	
-	
-	// Tests
-		// ====================================================
+	private RaffleService		raffleService;
 
-	protected void createAndSaveAuditReportTest(String username, int raffleId,String text,Class<?> expected) {
+
+	// Tests
+	// ====================================================
+
+	protected void createAndSaveAuditReportTest(final String username, final int raffleId, final String text, final Class<?> expected) {
 		Class<?> caught;
 
 		caught = null;
 		try {
 
-			authenticate(username);
-			Raffle raffle = this.raffleService.findOne(raffleId);
-			AuditReport auditReport = this.auditReportService.create();
+			this.authenticate(username);
+			final Raffle raffle = this.raffleService.findOne(raffleId);
+			final AuditReport auditReport = this.auditReportService.create();
 			auditReport.setMoment(new Date(System.currentTimeMillis() - 1));
 			auditReport.setRaffle(raffle);
 			auditReport.setText(text);
 			auditReport.setFinalMode(false);
 			this.auditReportService.save(auditReport);
-			
-			unauthenticate();
 
-		} catch (Throwable oops) {
+			this.unauthenticate();
+
+		} catch (final Throwable oops) {
 			caught = oops.getClass();
 		}
-		checkExceptions(expected, caught);
+		this.checkExceptions(expected, caught);
 
 	}
-	
+
 	@Test
 	public void driverCreateAndSaveAuditReport() {
 
-		Object testingData[][] = {
-			
+		final Object testingData[][] = {
+
 			{
-				"auditor1",129,"Texto para auditar", null
+				"auditor1", 129, "Texto para auditar", null
 			},
-			
+
 			{
-				"auditor1",123333,"Texto para auditar", ConstraintViolationException.class
-			},
-			{
-				"auditor1",129,"", ConstraintViolationException.class
-			},
-			{
-				"manager1",129,"Texto11111", ConstraintViolationException.class
+				"auditor1", 123333, "Texto para auditar", ConstraintViolationException.class
+			}, {
+				"auditor1", 129, "", ConstraintViolationException.class
+			}, {
+				"manager1", 129, "Texto11111", ConstraintViolationException.class
 			}
-			
-			
+
 		};
 
-		for (int i = 0; i < testingData.length; i++) {
-			createAndSaveAuditReportTest((String) testingData[i][0],(int) testingData[i][1],(String) testingData[i][2],(Class<?>) testingData[i][3]);
-		}
+		for (int i = 0; i < testingData.length; i++)
+			this.createAndSaveAuditReportTest((String) testingData[i][0], (int) testingData[i][1], (String) testingData[i][2], (Class<?>) testingData[i][3]);
 	}
-	
-	protected void deleteAuditReportTest(String username,int auditReportId,Class<?> expected) {
+
+	protected void deleteAuditReportTest(final String username, final int auditReportId, final Class<?> expected) {
 		Class<?> caught;
 
 		caught = null;
 		try {
 
-			authenticate(username);
-			
-			AuditReport auditReport = this.auditReportService.findOne(auditReportId);
-			
-			this.auditReportService.delete(auditReport);
-			
-			unauthenticate();
+			this.authenticate(username);
 
-		} catch (Throwable oops) {
+			final AuditReport auditReport = this.auditReportService.findOne(auditReportId);
+
+			this.auditReportService.delete(auditReport);
+
+			this.unauthenticate();
+
+		} catch (final Throwable oops) {
 			caught = oops.getClass();
 		}
-		checkExceptions(expected, caught);
+		this.checkExceptions(expected, caught);
 
 	}
-	
+
 	@Test
 	public void driverDeleteAuditReport() {
 
-		Object testingData[][] = {
-			
+		final Object testingData[][] = {
+
 			{
-				"auditor1",181, null
+				"auditor1", 181, null
 			},
-			
+
 			{
-				"auditor1",123333, IllegalArgumentException.class
-			},
-			{
-				"manager1",181, IllegalArgumentException.class
+				"auditor1", 123333, IllegalArgumentException.class
+			}, {
+				"manager1", 181, IllegalArgumentException.class
 			}
-			
-			
+
 		};
 
-		for (int i = 0; i < testingData.length; i++) {
-			deleteAuditReportTest((String) testingData[i][0],(int) testingData[i][1],(Class<?>) testingData[i][2]);
-		}
+		for (int i = 0; i < testingData.length; i++)
+			this.deleteAuditReportTest((String) testingData[i][0], (int) testingData[i][1], (Class<?>) testingData[i][2]);
 	}
-	
 
 }
